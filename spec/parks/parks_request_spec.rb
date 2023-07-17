74de26e237aeaba95bc10b4f4497e1b04d702f80
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-07-17 11:59:58 -0600

    visit root path select state from field test setup

--- spec/parks/parks_request_spec.rb
+++ spec/parks/parks_request_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Parks API' do
-  describe "authenticate API key" do
-    it "successfully authenticates API key" do
-      json_response = File.read('spec/fixtures/park_data.json')
-      response = stub_request(:get, "https://developer.nps.gov/api/v1/parks").
-        to_return(status: 200, body: json_response)
-      parks_parse = JSON.parse(response.response.body, symbolize_names: true)
-      parks_data = parks_parse[:data]
-      # require 'pry'; binding.pry
-      expect(parks_parse[:total]).to eq("470")
-      expect(parks_data).to be_an(Array)
-      expect(parks_data.count).to eq(50)
-    end
-  end
+  # describe "authenticate API key" do
+  #   it "successfully authenticates API key" do
+  #     json_response = File.read('spec/fixtures/park_data.json')
+  #     response = stub_request(:get, "https://developer.nps.gov/api/v1/parks").
+  #       to_return(status: 200, body: json_response)
+  #     parks_parse = JSON.parse(response.response.body, symbolize_names: true)
+  #     parks_data = parks_parse[:data]
+
+  #     expect(parks_parse[:total]).to eq("470")
+  #     expect(parks_data).to be_an(Array)
+  #     expect(parks_data.count).to eq(50)
+  #   end
+  # end
 
   describe "get all national park data" do
     it "gets list of all state names" do
-      json_response = File.read('spec/fixtures/park_data.json')
-      response = stub_request(:get, "https://developer.nps.gov/api/v1/parks").
-        to_return(status: 200, body: json_response)
-      parks_parse = JSON.parse(response.response.body, symbolize_names: true)
+      visit "/"
 
+      select "Tennessee",  from: "state"
+      click_on "Find Parks"
+      expect(current_path).to eq(parks_path)
     end
   end
 end
