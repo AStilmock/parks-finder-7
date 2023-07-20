ab6ae3f43d88c0d619b89deba3ce2e57e92d1d22
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-07-19 20:25:04 -0600

    IC Mid Mod Practice

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    json_response = File.read('spec/fixtures/park_data.json')
-    response = stub_request(:get, "https://developer.nps.gov/api/v1/parks").
-      to_return(status: 200, body: json_response)
-    parks_parse = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
   end
 end
---
+++ spec/features/parks_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Parks Search Home Page" do
+  it "has park search link" do
+    visit root_path
+  
+
+  end
+end
